4caa8cbd8a58707140a56eb8d76d91d003881265
qwellk <541003973@qq.com>
2017-09-26 03:07:53 +0800

    sep26,3:07

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Sudu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                //makesudu
+                if (args[1] == "-c" & Regex.IsMatch(args[2], @"^[+-]?/d*$"))
+                {
+
+                }
+
+                //dealsudu
+                else if (args[1] == "-s")
+                {
+                    if (File.Exists(args[2]))
+                    {
+                        Dealsudu ds = new Dealsudu(args[2]);
+                        
+                    }
+                    else return;
+                }
+            }
+            
+
+            else
+            {
+                return;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/dealsudu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sudu1
+{
+    class Dealsudu
+    {
+        string filename;
+        FileStream F;
+        StreamReader R;
+
+        public Dealsudu(string s)
+        {
+            filename = s; 
+            F = new FileStream(filename,FileMode.Open);
+            R = new StreamReader(F);
+        }
+
+        public void Deal()
+        {
+            string line;
+            while ((line = R.ReadLine()) != null)
+            {
+                char[] chars = line.ToCharArray();
+                int[,] juzhen=new int[9,9];
+                for(int i=1;i<82 ;i++)
+                {
+                    juzhen[i % 9,i / 9] = Convert.ToInt32(chars[i]);
+                }
+                //juzhen is already
+                
+            }
+        }
+
+        private void Solve()
+        {
+
+        }
+
+    }
+}
